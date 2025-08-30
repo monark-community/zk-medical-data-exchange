93ca27c7610e9bfc8270c4f8c91e4a17aa895fd5
Paul Petibon <paul.j.petibon@gmail.com>
2025-08-30 18:41:12 -0400

    chore: added nextjs web app

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+      <Link href={"https://example.com"}>
+        <Button variant={"default"}>Example link</Button>
+      </Link>
+      <p className="mt-4 text-lg">This is a simple Next.js application.</p>
+    </main>
+  );
+}
