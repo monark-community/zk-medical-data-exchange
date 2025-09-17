869153ca207db41dd14fde91aa17f324e743ffd0
laureltremblay <78708778+liotrique@users.noreply.github.com>
2025-09-16 21:55:22 -0400

    fix: fixed ci env vars logic

--- apps/api/src/config/config.ts
+++ apps/api/src/config/config.ts
@@ -1,15 +1,21 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-function requireEnv(name: string): string {
+function requireEnv(name: string): string | undefined {
+  if (process.env.CI) return undefined;
   const value = process.env[name];
   if (!value) throw new Error(`Missing required env var: ${name}`);
   return value;
 }
 
+function determineLocalMode(): boolean {
+  if (process.env.CI) return true;
+  return process.env.IS_LOCAL_MODE === "true";
+}
+
 export const Config = {
+  IS_LOCAL_MODE: determineLocalMode(),
   APP_API_KEY: requireEnv("APP_API_KEY"),
-  IS_IN_TESTING_MODE: process.env.IS_TESTING === "true" || false,
   // Supabase
   SUPABASE_URL: requireEnv("SUPABASE_URL"),
   SUPABASE_KEY: requireEnv("SUPABASE_ANON_KEY"),
--- apps/web/utils/walletKey.ts
+++ apps/web/utils/walletKey.ts
@@ -18,14 +18,19 @@ declare global {
 /**
  * Derive a symmetric AES key from a wallet signature.
  * This key will always be the same for the same wallet.
+ *
+ * @returns {Promise<Buffer>} A promise that resolves to a 32-byte AES key.
  */
 export const deriveKeyFromWallet = async (): Promise<Buffer> => {
   if (!window.ethereum) throw new Error("MetaMask not found");
 
   const provider = new BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
+  
+  // get address (public wallet id)
+  const address = await signer.getAddress();
+  console.log("Wallet address:", address);
   const message = "Generate AES key for encryption";
-
   const signature = await signer.signMessage(message);
 
   // Hash the signature to get a 32-byte AES key
