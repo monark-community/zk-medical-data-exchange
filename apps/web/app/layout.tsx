61d1f76cf0ca6a0b2ecae1a95dedcd436f153bb0
benjaminpiche8 <benjamin.piche@polymtl.ca>
2025-09-17 14:21:26 -0400

    feat: basic connect/disconnect flow using web3auth

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,22 @@
+import Provider from "@/components/provider";
+import { Metadata } from "next";
+import { cookieToWeb3AuthState } from "@web3auth/modal";
+import { headers } from "next/headers";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Cura",
+  description: "A ZK-Powered Medical Data Exchange",
+};
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const headersList = await headers();
+  const web3authInitialState = cookieToWeb3AuthState(headersList.get('cookie'));
+  return (
+    <html lang="en">
+      <body>
+        <Provider web3authInitialState={web3authInitialState}>{children}</Provider>
+      </body>
+    </html>
+  );
+}
