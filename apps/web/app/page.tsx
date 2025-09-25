ebf08bf97785f12cdf53667bb8ecfab66f50166b
benjaminpiche8 <benjamin.piche@polymtl.ca>
2025-09-25 15:17:40 -0400

    feat: nonce signature, more basic version

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useWeb3AuthConnect } from "@web3auth/modal/react";
 import { requestNonce, verifySignature } from "@/services/authService";
-import { ethers } from "ethers";
+import { BrowserProvider } from "ethers";
 import { useRouter } from "next/navigation";
 
 export default function LandingPage() {
@@ -14,29 +14,23 @@ export default function LandingPage() {
   const handleConnectionAttempt = async () => {
     try {
       setIsAuthenticating(true);
-      console.log("Starting authentication flow...");
       
       const web3authProvider = await connect();
-      
-      if (!web3authProvider) {
-        setIsAuthenticating(false);
-        return;
-      }
 
-      const provider = new ethers.BrowserProvider(web3authProvider);
+      if (!web3authProvider) { throw new Error("MetaMask not found"); }
+
+      const provider = new BrowserProvider(web3authProvider);
       const signer = await provider.getSigner();
       const userAddress = await signer.getAddress();
       
       const { message } = await requestNonce(userAddress);
       const signature = await signer.signMessage(message);
+      const res = await verifySignature(userAddress, signature, message);
 
-      const verificationResult = await verifySignature(userAddress, signature, message);
-
-      if (verificationResult.success) {
-        console.log("Authentication successful! Redirecting to dashboard...");
+      if (res.success) {
         router.push('/dashboard');
       } else {
-        console.error("Authentication failed:", verificationResult.error);
+        throw new Error("Invalid signature");
       }
     } catch (err) {
       console.log("Authentication failed:", err);
