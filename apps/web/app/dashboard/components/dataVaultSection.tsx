1483ed810ea47be7ee625e8ab57cae174c979f9e
Alexandre Veillette <alexandreveillette.av@gmail.com>
2025-09-26 20:28:56 -0400

    feat: create all Dasboard section

---
+++ apps/web/app/dashboard/components/dataVaultSection.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const DataVaultSection = () => {
+  return <h1></h1>;
+};
+
+export default DataVaultSection;
---
+++ apps/web/app/dashboard/components/overviewSection.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const OverviewSection = () => {
+  return <h1></h1>;
+};
+
+export default OverviewSection;
