0d5340dddf3b531bde5f4a52ca07ab4bbabd80f6
Alexandre Veillette <alexandreveillette.av@gmail.com>
2025-09-30 17:21:27 -0400

    chore: palce UI constant in is folder

--- apps/web/app/dashboard/components/accountOverview.tsx
+++ apps/web/app/dashboard/components/accountOverview.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { ACCOUNT_OVERVIEW_UI } from "@/app/dashboard/constants/UI";
 
 // TODO fetch real data from backend
 const accountInfo = {
@@ -11,40 +12,7 @@ const accountInfo = {
   privacyScore: 100 + "%",
 };
 
-const ui = {
-  totalEarned: {
-    icon1: "Coins",
-    icon1Color: "text-blue-600",
-    icon2: "TrendingUp",
-    icon2Color: "text-green-600",
-    title: "Total Earned",
-    color: "from-blue-50 to-blue-100 border-blue-200",
-  },
-  nbDataPointsShared: {
-    icon1: "Database",
-    icon1Color: "text-teal-600",
-    icon2: "Activity",
-    icon2Color: "text-green-600",
-    title: "Data Points Shared",
-    color: "from-teal-50 to-teal-100 border-teal-200",
-  },
-  nbActiveStudies: {
-    icon1: "BookOpenText",
-    icon1Color: "text-purple-600",
-    icon2: "RefreshCcw",
-    icon2Color: "text-green-600",
-    title: "Active Studies",
-    color: "from-purple-50 to-purple-100 border-purple-200",
-  },
-  privacyScore: {
-    icon1: "ShieldCheck",
-    icon1Color: "text-green-600",
-    icon2: "Lock",
-    icon2Color: "text-green-600",
-    title: "Privacy Score",
-    color: "from-green-50 to-green-100 border-green-200",
-  },
-};
+const ui = ACCOUNT_OVERVIEW_UI;
 const AccountOverview = () => {
   return (
     <div className="grid md:grid-cols-4 gap-6 mb-8">
