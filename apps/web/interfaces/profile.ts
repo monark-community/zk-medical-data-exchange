c368a46053cd2df2cf9c001c022d7d46b373127f
Alexandre Veillette <alexandreveillette.av@gmail.com>
2025-10-15 18:36:45 -0400

    feat: add profileCardProps interface example

---
+++ apps/web/interfaces/profile.ts
@@ -0,0 +1,8 @@
+export interface ProfileCardProps {
+  walletAddress?: string;
+  userAlias?: string;
+  accountType?: string;
+  dataContributions?: number;
+  earnings?: number;
+  privacyScore?: number;
+}
