ef740d55f25d03e56b95d420b80d2595d0c0e9e9
Alexandre Veillette <alexandreveillette.av@gmail.com>
2025-10-20 16:31:15 -0400

    fix: profil card not displaying on Reseacher

--- apps/web/app/profile/components/profileCard.tsx
+++ apps/web/app/profile/components/profileCard.tsx
@@ -37,7 +37,7 @@ const ProfileCard = () => {
     fetchUserData();
   }, [fetchUserData]);
   React.useEffect(() => {
-    if (user && currentProfile) {
+    if (user) {
       setProfileCardInfo({
         walletAddress: user.id,
         userAlias: user.username,
