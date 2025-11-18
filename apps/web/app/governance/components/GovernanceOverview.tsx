13b6da27338d8a6c24f24afeb7bf83573414b6f5
Alexandre Veillette <alexandreveillette.av@gmail.com>
2025-11-18 14:54:18 -0500

    feat: implement useGovernance hook and integrate governance stats in GovernanceOverview component

--- apps/web/app/governance/components/GovernanceOverview.tsx
+++ apps/web/app/governance/components/GovernanceOverview.tsx
@@ -3,20 +3,35 @@ import React from "react";
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { GOVERNANCE_OVERVIEW_UI } from "@/app/governance/constants/UI";
-
-// TODO fetch real data from backend
-const governanceInfo = {
-  totalProposals: 47,
-  nbActiveVoters: 12543,
-  avgParticipationPercentage: 78 + "%",
-  votingPower: 5.4 + "M", //quantité de vote totale?
-};
+import { useGovernance } from "@/hooks/useGovernance";
 
 const ui = GOVERNANCE_OVERVIEW_UI;
 const GovernanceOverview = () => {
+  const { stats, isLoading } = useGovernance();
+  console.log("Governance stats in component:", stats);
+  const governanceStats = {
+    totalProposals: stats?.totalProposals ?? 0,
+    nbActiveVoters: stats?.uniqueVoters ?? 0,
+    avgParticipationPercentage: (stats?.avgParticipation ?? 0) + "%",
+    votingPower: stats?.votingPower ?? 0,
+  };
+
+  if (isLoading) {
+    return (
+      <div className="grid md:grid-cols-4 gap-6 mb-8">
+        {Object.keys(ui).map((key) => (
+          <Card key={key} className="rounded-lg border bg-card animate-pulse">
+            <CardHeader className="flex items-center align-center justify-center h-16" />
+            <CardContent className="justify-center h-20" />
+          </Card>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="grid md:grid-cols-4 gap-6 mb-8">
-      {Object.entries(governanceInfo).map(([key, value]) => {
+      {Object.entries(governanceStats).map(([key, value]) => {
         const uiKey = key as keyof typeof ui;
         return (
           <Card
