68fb3e7edb58c71e4be4a0fe598b7bc656410794
Paul Petibon <paul.j.petibon@gmail.com>
2025-11-24 09:56:50 -0500

    fix bug UI bug when the user would upload a non compliant file

--- apps/web/utils/fhir.ts
+++ apps/web/utils/fhir.ts
@@ -16,7 +16,7 @@ export async function isFhirCompliant(file: File): Promise<FhirResourceTypes> {
   }
 
   if (!json || typeof json !== "object" || !("resourceType" in json)) {
-    throw new Error("The uploaded file does not appear to be a valid FHIR resource.");
+    return FhirResourceType.NOT_SUPPORTED;
   }
 
   const resourceType = json.resourceType;
